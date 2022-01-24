6a679817e564e7f6e65d8cce8978206dd5722f41
leegandhi18 <leegandhi18@gmail.com>
2022-01-24 15:04:12 +0900

    Setting the environment

--- app.js
+++ app.js
@@ -1,34 +1,42 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+// const logger = require('morgan'); // 구코드 삭제
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const corsConfig = require('./config/corsConfig.json');
+const logger = require('./lib/logger');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+logger.info('app start');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+// app.use(logger('dev')); // 구코드 삭제
+app.use(cors(corsConfig));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const logger = require('../lib/logger');
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
+});
+
+// logTest
+router.get('/log-test', (req, res, next) => {
+  logger.error('This message is error');
+  logger.warn('This message is warn');
+  logger.info('This message is info');
+  logger.verbose('This message is verbose');
+  logger.debug('This message is debug');
+  logger.silly('This message is silly');
+
+  res.send('log test');
 });
 
 module.exports = router;
