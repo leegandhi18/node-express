3fd66bacc79684e0989f2621eecb486b15c4b374
leegandhi18 <leegandhi18@gmail.com>
2022-01-26 18:36:05 +0900

    Using crypto test for password

--- service/userService.js
+++ service/userService.js
@@ -1,13 +1,32 @@
 const logger = require('../lib/logger');
 const userDao = require('../dao/userDao');
+const hashUtil = require('../lib/hashUtil');
 
 const service = {
   // user 입력
   async reg(params) {
     let inserted = null;
 
+    // 1. 비밀번호 암호화
+    let hashPassword = null;
     try {
-      inserted = await userDao.insert(params);
+      hashPassword = await hashUtil.makePasswordHash(params.password);
+      logger.debug(`(userService.makePassword) ${JSON.stringify(params.password)}`);
+    } catch (err) {
+      logger.error(`(userService.makePassword) ${err.toString()}`);
+      return new Promise((resolve, reject) => {
+        reject(err);
+      });
+    }
+
+    // 2. 사용자 등록 처리
+    const newParams = {
+      ...params,
+      password: hashPassword,
+    };
+
+    try {
+      inserted = await userDao.insert(newParams);
       logger.debug(`(userService.reg) ${JSON.stringify(inserted)}`);
     } catch (err) {
       logger.error(`(userService.reg) ${err.toString()}`);
